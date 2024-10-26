483f7542d9d8ada819d1d057121e6ba90c3c52ab
Guilherme Alves <97guilherme.alves@gmail.com>
2024-10-26 14:29:51 -0300

    :tada: initial commit

---
+++ src/server.ts
@@ -0,0 +1,87 @@
+import express from "express";
+import http from "http";
+import QRCode from "qrcode";
+import cors from "cors";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+// Configuração do CORS
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+interface QRCodeStatus {
+  locked: boolean;
+  imageUrl: string;
+}
+
+interface QRCodes {
+  [key: string]: QRCodeStatus;
+}
+
+// ID fixo do QR Code
+const fixedQRCodeId = "myFixedQRCode";
+let qrCodes: QRCodes = {
+  [fixedQRCodeId]: { locked: true, imageUrl: "" },
+};
+
+let qrStatus = { locked: true, redirectUrl: "/bloqueado" };
+
+// Rota para desbloquear QR Code
+app.post("/unlock-qr", (req, res) => {
+  const { qrId } = req.body;
+  if (qrId === fixedQRCodeId) {
+    qrCodes[fixedQRCodeId].locked = false;
+    qrStatus.locked = false;
+    const status = {
+      locked: false,
+      redirectUrl: "https://psiuu-03.vercel.app/",
+    };
+
+    io.emit("qrUnlocked", status);
+    res.send("QR Code desbloqueado!");
+  } else {
+    res.status(404).send("QR Code não encontrado.");
+  }
+});
+
+// Rota para bloquear QR Code
+app.post("/lock-qr", (req, res) => {
+  const { qrId } = req.body;
+  if (qrId === fixedQRCodeId) {
+    qrCodes[fixedQRCodeId].locked = true;
+    qrStatus.locked = true;
+    const status = { locked: true, redirectUrl: "/bloqueado" };
+
+    io.emit("qrLocked", status);
+    res.send("QR Code bloqueado!");
+  } else {
+    res.status(404).send("QR Code não encontrado.");
+  }
+});
+
+// Evento de conexão do Socket.IO
+io.on("connection", (socket) => {
+  console.log("Cliente conectado:", socket.id);
+  socket.emit("qrStatus", qrStatus);
+});
+
+// Inicia o servidor na porta 5050
+server.listen(5050, () => {
+  console.log("Servidor rodando na porta 5050");
+});
